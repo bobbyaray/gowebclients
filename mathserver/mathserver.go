5b83ab0843dcd802867c3c1e1e37e8395f3eec53
bobbyaray <bobby.a.ray@gmail.com>
2018-03-26 21:41:23 -0400

    Added method for squaring numbers.

--- mathserver/mathserver.go
+++ mathserver/mathserver.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("Starting Math Server......")
+	http.HandleFunc("/squared", squaredHandler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func squaredHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Called!")
+	r.ParseForm()
+	value := r.Form["value"]
+	numvalue, _ := strconv.Atoi(value[0])
+	fmt.Println("Squaring " + value[0])
+	fmt.Fprintf(w, "%d", numvalue*numvalue)
 }
